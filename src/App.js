b509a828ebc5c517d022c29077fbe598e633015c
Mohamed Ali AMOR <amormedali92@gmail.com>
2021-03-22 09:21:46 +0100

    children prop img fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Profile fullName="Mohamed Ali Amor" bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." profession="full Stack Developer" handleName={handleName}>
-     
+        <img src="images/profil-pic.jpg" alt="" style={{height:350, width:300,position:"absolute", left:"10vw", top:20, borderRadius:10}} / >
       </Profile>
     </div>
   );
--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-function Profile({fullName, bio, profession, handleName }){
+function Profile({fullName, bio, profession, handleName, children }){
     return(
 <div  style={{position:"relative" ,padding:100 ,display:"flex", justifyContent:"center", alignItems:"center", backgroundColor:"#ececec"}}>
         {handleName(fullName)}
-        <img src="/images/profil-pic.jpg" alt="" style={{height:350, width:300,position:"absolute", left:"10vw", top:20, borderRadius:10 }} />
-
+        {/* <img src="/images/profil-pic.jpg" alt="" style={{height:350, width:300,position:"absolute", left:"10vw", top:20, borderRadius:10 }} /> */}
+      {children}
     <div style={{display:"flex", flexDirection:"column", height:"60vh", width:"50vw",borderRadius:10, backgroundColor:"#fff", paddingLeft:"30%", paddingRight:"3%"  }}>
     <h1>{fullName}</h1>
     <h3 style={{ }}>{profession}</h3>
